1a3d8c79f16835ba4fca02271a464b54adf00f86
Kristoffer Karlsson <krika694@student.liu.se>
2017-05-04 14:22:31 +0200

    userinput

---
+++ Game/UserInput.h
@@ -0,0 +1,42 @@
+#ifndef USERINPUT_H
+#define USERINPUT_H
+#include <SFML/Graphics.hpp>
+
+
+
+class UserInput
+{
+public:
+    bool up, down, left, right;
+    
+    UserInput() : up(false), down(false), left(false), right(false) {}
+
+    void onKey(bool pressed, sf::Keyboard::Key key) {
+	switch (key) { 	
+	case sf::Keyboard::Up: 	
+	case sf::Keyboard::W: 	
+	    up = pressed; 		
+	    break; 	
+	case sf::Keyboard::Down: 
+	case ::sf::Keyboard::S: 
+	    down = pressed; 
+	    break; 	
+	case sf::Keyboard::Left: 
+	case sf::Keyboard::A: 	
+	    left = pressed; 	
+	    break; 		case sf::Keyboard::Right: 
+	case sf::Keyboard::D: 		
+	    right = pressed; 		
+	    break; 	
+	default: 
+	    break; 	
+	} 
+    }
+
+
+
+private:
+
+}
+
+#endif
