ab3f2966799562dc58548056cccd3cfe9f57a52f
August Johnson <augjo318@student.liu.se>
2017-05-04 13:49:50 +0200

    updated main

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,13 +1,25 @@
-#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
 
 int main()
 {
-  sf::Window window(sf::VideoMode(800, 600), "My window");
+  sf::View Box;
+  Box.reset(sf::FloatRect(200, 200, 300, 200));
+  Box.setCenter(sf::Vector2f(350, 300));
+
+  sf::Texture texture;
+  texture.loadFromFile("background2.png");
+  sf::RectangleShape rectangle(sf::Vector2f(150, 80));
+  sf::RenderWindow window(sf::VideoMode(1024, 768), "My window", 
+			  sf::Style::Close | sf::Style::Resize);
+  
+  window.setVerticalSyncEnabled(true);
 
   // run the program as long as the window is open
   while (window.isOpen())
   {
-    // check all the window's events that were triggered since the last iteration of the loop
+
     sf::Event event;
     while (window.pollEvent(event))
     {
@@ -15,7 +27,15 @@ int main()
       if (event.type == sf::Event::Closed)
 	window.close();
     }
+
+    window.clear(sf::Color::Blue);
+    sf::Sprite background(texture);
+    //background.setScale(3.0f, 1.0f);
+    window.draw(rectangle);
+    window.draw(background);
+    window.display();
   }
+
 
   return 0;
 }
