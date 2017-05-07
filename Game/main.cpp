cdafa39a174e2a1415421e12bcb863c299ab5de5
Gustav Ljungberg <guslj314@student.liu.se>
2017-05-07 14:49:19 +0200

    hej

--- Game/main.cpp
+++ Game/main.cpp
@@ -26,6 +26,8 @@ int main()
     sf::RectangleShape rec(sf::Vector2f(40.0f, 40.0f)); 
     rec.setOrigin(20.0f, 20.0f); 
     rec.setFillColor(sf::Color::Red);
+    sf::RectangleShape ground(sf::Vector2f(10000.0f, 50.0f)); // new
+    ground.setPosition(50,300);
 
     Enemy enemy(sf::Vector2f(400, 400), 100, 100, 10, 10, &playerTexture, sf::Vector2i(3, 2), 0.2f);
 
@@ -60,20 +62,32 @@ int main()
 		    cout << "ASCII chaaracter typed: " << static_cast<char>(event.text.unicode) << endl;
 	    }
 	}
-	rec.move(50*deltaTime, 50*deltaTime);
-
 	// Update objects
 	enemy.Update(deltaTime);
 	
 	view.setCenter(rec.getPosition().x, 0);
 // Objects rendered before clear will not be visible
 	window.clear();
-//-----------------------------
+	//-----------------------------
 	window.draw(rec);
+	window.draw(ground); // new
 	enemy.Draw(window);
-
 	window.setView(view);
 	window.display();
+	//set and update the bounding box 
+	sf::FloatRect boundingBox = ground.getGlobalBounds();
+	sf::FloatRect OtherBox = rec.getGlobalBounds();
+	//check for collision
+	if(boundingBox.intersects(OtherBox))
+	  {
+	    rec.move(0,0);
+	  }
+	else
+	  {
+	    rec.move(50*deltaTime, 50*deltaTime);
+	  }
+		
+
     }
 	
     window.close();
