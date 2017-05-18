0bc29263d98e80a85819a2af75edb89da825d43d
Alexander Eriksson <aleer778@student.liu.se>
2017-05-18 22:57:08 +0200

    Fixes

--- Game/Collider.h
+++ Game/Collider.h
@@ -1,18 +1,20 @@
-#pragma once
+#ifndef COLLIDER_H
+#define COLLIDER_H
 #include <SFML/Graphics.hpp>
 
 class Collider
 {
 public:
     Collider(sf::RectangleShape & body);
-    ~Collider();
 
     void Move(float dx, float dy) { body.move(dx, dy); }
     bool CheckCollision(Collider & other, sf::Vector2f & direction);
 
-    sf::Vector2f GetPosition() { return body.getPosition(); }
+    sf::Vector2f GetPosition() { return body.getPosition(); } 
     sf::Vector2f GetSize() { return body.getSize(); }
 
 private:
     sf::RectangleShape & body;
 };
+
+#endif
